469f2aaf93b67648b030b6f7b34bf0ce5f401afc
fathallah.houssem <houssem.fathallah@devoteam.com>
2021-07-04 01:26:43 +0200

    init skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
+import { FormattedMessage, IntlProvider } from 'react-intl';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import logo from './images/logo.svg';
+// change layout given the auth state
+import { DashboardLayout } from "./modules/shared/layouts";
+import i18nMessages from "./i18n";
+import { IRootState } from "./modules/shared/redux/store";
+import { Flex, Typography } from '@aircall/tractor';
+
+interface IProps {
+  locale: string
 }
 
-export default App;
+const Greeting = () => <FormattedMessage id="common.welcome"/>;
+
+class App extends React.Component<IProps> {
+  render() {
+    return (
+        <IntlProvider messages={i18nMessages[this.props.locale]} locale={this.props.locale}
+                      defaultLocale={this.props.locale}>
+          <DashboardLayout>
+            <Router>
+              <Switch>
+                <Route path={"/"}>
+                  <Flex flexDirection="column" justifyContent="center">
+                    <img src={logo} alt="logo"/>
+                    <Typography mx="auto" pt="4" variant="heading2">
+                      <Greeting/>🙂
+                    </Typography>
+                    <Link to={"/"}></Link>
+                  </Flex>
+                </Route>
+              </Switch>
+            </Router>
+          </DashboardLayout>
+        </IntlProvider>
+    );
+  }
+}
+
+/*
+
+const mapStateToProps: MapStateToProps<any, any> = (state: any) => ({
+    locale: state.intlReducer.locale
+})
+
+ */
+const mapStateToProps = (state: IRootState) => {
+  return {
+    locale: state.intlReducer.locale
+  }
+};
+export default connect(mapStateToProps)(App)
+
